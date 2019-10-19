96da05e1143a69889ae0f088028e73020ee0ecb7
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-19 21:28:13 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -27,13 +27,14 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     if cart[item] 
      
-      if cart[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+      if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
        cart["#{item} W/COUPON"] = {price: coupon[:cost] /coupon[:num] , clearance: cart[item][:clearance] , count: coupon[:num] }
        cart[item][:count] -= coupon[:num]
        
-     elsif cart[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
+     elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
            cart["#{item} W/COUPON"][:count] += coupon[:num]
            cart[item][:count] -= coupon[:num]
+   end
    end
   end 
   return cart
@@ -43,9 +44,18 @@ end
 
 
 def apply_clearance(cart)
-  # code here
+ cart.each do |item , info|
+ info[:price] -= info[:price] * 0.2 if info[:clearance] == true
+ end
+ return cart
 end
 
 def checkout(cart, coupons)
-  # code here
-end
+  hash = consolidate_cart(cart)
+  coupon_discounts = apply_coupons(hash, coupons)
+  clearance = apply_clearance(coupon_discounts)
+  
+ total = clearance.reduce(0) {| acc, (key, value)| acc += value[:price] * value[:count] }
+ total > 100 ? total * 0.9 : total
+ 
+ end
